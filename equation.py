5c90f07284c22509d9a6afe681db12c7ed0bed0a
VolodymyrKretsul <volodymyr.kretsul.ki.2020@lpnu.ua>
2024-09-23 16:00:18 +0300

    Видалено файли

--- equation.py
+++
@@ -1,9 +0,0 @@
-import math
-
-x = 5.168
-z = 1.569
-y = 3.022
-
-result = (x + 1)**2 + 2 * (y + z) / (x + y - z**2) + 13 * math.log(x * y + z, 5)
-
-print(result)
--- task3.py
+++
@@ -1,5 +0,0 @@
-is_sunny = True
-is_cloudy = True
-
-results = is_sunny or is_cloudy
-print(results)
